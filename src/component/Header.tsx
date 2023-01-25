a32604b158eaff203db18efab543d8637fa4a8f2
ZidaneDanukusuma514 <56733493+ZidaneDanukusuma514@users.noreply.github.com>
2023-01-25 23:01:53 +0700

    Add Header

---
+++ src/component/Header.tsx
@@ -0,0 +1,29 @@
+import * as React from "react";
+
+export interface IHeaderProps {}
+
+export default function Header(props: IHeaderProps) {
+  return (
+    <header className="flex items-center py-4 bg-slate-800">
+      <h1 className="font-bold text-white text-2xl border-r-[1px] px-4">
+        Zidane
+      </h1>
+      <nav className="grow">
+        <ul className="flex space-x-4 mx-4">
+          <li className="font-bold text-white/75 hover:text-white mx-4">
+            Past work
+          </li>
+          <li className="font-bold text-white/75 hover:text-white mx-4">
+            Skills
+          </li>
+          <li className="font-bold text-white/75 hover:text-white mx-4">
+            Testimonial
+          </li>
+        </ul>
+      </nav>
+      <button className="font-bold text-white/75 hover:text-white mx-4">
+        Hire Me →
+      </button>
+    </header>
+  );
+}
