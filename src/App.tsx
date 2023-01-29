a42929d5a5ab7e8866253f1c5049a8ef70457e9e
ZidaneDanukusuma514 <56733493+ZidaneDanukusuma514@users.noreply.github.com>
2023-01-29 20:23:53 +0700

    add axios fact cat

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,22 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import reactLogo from "./assets/react.svg";
 import "./App.css";
 import Hero from "./section/Hero";
 import Skills from "./section/Skill";
 import Projects from "./section/Projects";
+import axios from "axios";
 
 function App() {
   const [count, setCount] = useState(0);
-
+  useEffect(() => {
+    axios("https://catfact.ninja/fact")
+      .then((res) => {
+        console.log("Data: ", res.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
   return (
     <div className="bg-slate-800">
       <Hero />
