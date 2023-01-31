3628dfa41fb30b4195972d1665f13ad9d7874046
ZidaneDanukusuma514 <56733493+ZidaneDanukusuma514@users.noreply.github.com>
2023-01-31 21:08:52 +0700

    Fixing Grid

--- src/section/Skill.tsx
+++ src/section/Skill.tsx
@@ -7,16 +7,24 @@ export default function Skills(props: ISkillsProps) {
     <section className="container mx-auto h-screen px-12 bg-slate-800">
       <h1 className={`${style.title} text-center font-mono`}>Skills</h1>
       <div className="flex h-full space-x-12 items-center justify-center ">
-        <div className="grid grid-cols-4 content-evenly justify-items-center flex-1 h-[80%] rounded-lg bg-slate-600">
-          <div className={`${style["skill-box"]}  bg-sky-800`}>ReactJS</div>
-          <div className={`${style["skill-box"]}  bg-sky-800`}>NextJS</div>
-          <div className={`${style["skill-box"]}  bg-sky-800`}>Typescript</div>
-          <div className={`${style["skill-box"]}  bg-sky-800`}>Firebase</div>
-          <div className={`${style["skill-box"]}  bg-sky-800`}>Tailwind</div>
-          <div className={`${style["skill-box"]}  bg-sky-800`}>GIT</div>
+        <div className="grid grid-cols-4 gap-4 content-evenly  justify-items-center h-[80%] rounded-lg bg-slate-600">
+          <div
+            className={` col-span-full py-4 w-full text-center bg-sky-800 text-white font-bold`}
+          >
+            ReactJS
+          </div>
+          <div className={`${style.skill}  bg-red-800`}>NextJS</div>
+          <div className={`${style.skill}  bg-slate-800`}>Typescript</div>
+          <div className={`${style.skill}  bg-sky-800`}>Firebase</div>
+          <div className={`${style.skill}  bg-sky-800`}>Tailwind</div>
+          <div
+            className={`w-full py-4 col-span-full bg-sky-800 text-center text-white font-bold`}
+          >
+            GIT
+          </div>
         </div>
         <div className="flex-1 h-[80%] rounded-lg bg-transparent">
-          <h1 className="text-lg text-white">title</h1>
+          <h1 className="text-2xl text-white">title</h1>
           <p>Explain here...</p>
         </div>
       </div>
